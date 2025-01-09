6a2ee0680115f7d7f48340c2b2acbc4e827b56fe
lastforever457 <lastforever457@gmail.com>
2025-01-10 02:54:44 +0500

    Send message functions

--- src/axios.ts
+++ src/axios.ts
@@ -1,11 +1,24 @@
 import axios from 'axios'
 
-const BASE_URL = 'http://localhost:3000'
+// const token = '7605634228:AAECY3fDGlaqfn_cotZyUxSh0f-ons9XKGk' //utax test
+const token = '7363986636:AAGyNRYY0sTjewZ3WqB06fGXSorQEzuBEM4' //utax test 2
+// const token = `7917403316:AAGvI4WMUMQB3CzASZX0z6kd9TVyhn4HlaY` //utax messanger
+
+// const BASE_URL = 'http://localhost:3000'
+const BASE_URL = 'https://utax-messenger-backend.onrender.com'
+const API_URL = `https://api.telegram.org/bot${token}`
 
 export const api = axios.create({
     baseURL: BASE_URL,
     headers: {
         'Content-Type': 'application/json',
         admin: 1541727357,
+    },
+})
+
+export const telegramApi = axios.create({
+    baseURL: API_URL,
+    headers: {
+        'Content-Type': 'application/json',
     },
 })
--- src/pages/message.tsx
+++ src/pages/message.tsx
@@ -1,20 +1,15 @@
-import { useMutation } from '@tanstack/react-query'
+import { useMutation, useQuery } from '@tanstack/react-query'
 import { Button, Form, Select, TimePicker } from 'antd'
 import TextArea from 'antd/es/input/TextArea'
 import { useCallback, useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
-import { api } from '../axios'
+import { api, telegramApi } from '../axios'
 import { weekdays } from '../types'
 
 const Message = () => {
-    const { mutate } = useMutation({
-        mutationKey: ['add-message'],
-        mutationFn: async (data: Record<string, any>) => {
-            await api.post('/message/create', { data })
-        },
-    })
-    const [messageInterval, setMessageInterval] = useState<string>('ONCE')
     const [form] = Form.useForm()
+    const [sendTime, setSendTime] = useState(form.getFieldValue('sendTime'))
+    const [messageInterval, setMessageInterval] = useState<string>('ONCE')
     const [selectedWeekdays, setSelectedWeekdays] = useState<weekdays[]>([
         'MONDAY',
         'TUESDAY',
@@ -23,24 +18,126 @@ const Message = () => {
         'FRIDAY',
     ])
 
+    const { mutate } = useMutation({
+        mutationKey: ['add-message'],
+        mutationFn: async (data: Record<string, any>) => {
+            try {
+                await api.post('/message/create', { data })
+            } catch (error) {
+                console.error('Error creating message:', error)
+                throw new Error('Failed to create message')
+            }
+        },
+    })
+
+    const { data: groups = [] } = useQuery({
+        queryKey: ['groups'],
+        queryFn: async () => {
+            const response = await api.post('/group/find-many', {})
+            return response.data
+        },
+    })
+
     useEffect(() => {
         form.setFieldsValue({
             weekdays: selectedWeekdays,
             interval: messageInterval,
         })
-    }, [])
+    }, [form, selectedWeekdays, messageInterval])
+
+    useEffect(() => {
+        let intervalId: NodeJS.Timeout
+        if (sendTime) {
+            const checkTime = () => {
+                const currentTime = new Date().toISOString().slice(11, 16) // Only hours and minutes
+                if (currentTime === sendTime) {
+                    try {
+                        for (const group of groups) {
+                            telegramApi.post('/sendMessage', {
+                                chat_id: `-${group.groupId}`,
+                                text: form.getFieldValue('message'),
+                            })
+                        }
+                        console.log('Messages sent successfully!')
+                        clearInterval(intervalId) // Stop the interval after sending messages
+                    } catch (error) {
+                        console.error('Error sending messages:', error)
+                    }
+                }
+            }
+            intervalId = setInterval(checkTime, 1000) // Check time every second
+        }
+
+        return () => clearInterval(intervalId) // Clean up on component unmount
+    }, [sendTime, groups, form])
+
     const onFinish = useCallback(
         (values: Record<string, any>) => {
-            if (messageInterval !== 'Bir marta') {
-                mutate({
-                    text: values.message,
-                    sendTime: values.sendTime.format('HH:mm'),
-                    interval: messageInterval,
-                })
+            console.log('Form submitted:', values)
+
+            const sendData = {
+                text: values.message,
+                sendTime: values.sendTime.format('HH:mm'),
+                interval: values.interval,
+                weekday: values.weekdays ? values.weekdays : null,
+            }
+
+            if (values.interval !== 'ONCE') {
+                console.log('Sending data:', sendData)
+
+                const now = new Date()
+                const sendTime = new Date()
+                const [hours, minutes] = values.sendTime
+                    .format('HH:mm')
+                    .split(':')
+                sendTime.setHours(parseInt(hours), parseInt(minutes), 0)
+
+                const timeDifference = sendTime.getTime() - now.getTime()
+                const delay = timeDifference > 0 ? timeDifference : 0
+
+                setTimeout(() => {
+                    // Logic to send the message
+                    console.log('Message sent:', sendData)
+                    mutate(sendData)
+
+                    if (values.interval !== 'ONCE') {
+                        const intervalId = setInterval(
+                            () => {
+                                console.log('Message sent:', sendData)
+                            },
+                            values.interval === 'DAILY' ? 86400000 : 604800000
+                        ) // Daily or Weekly
+
+                        // Clean up interval on component unmount
+                        return () => clearInterval(intervalId)
+                    }
+                }, delay)
+            } else {
+                const now = new Date()
+                const sendTime = new Date()
+                const [hours, minutes] = values.sendTime
+                    .format('HH:mm')
+                    .split(':')
+                sendTime.setHours(parseInt(hours), parseInt(minutes), 0)
+
+                const timeDifference = sendTime.getTime() - now.getTime()
+                const delay = timeDifference > 0 ? timeDifference : 0
+
+                setTimeout(() => {
+                    // Logic to send the message
+                    console.log('Message sent:', sendData)
+                    for (const group of groups) {
+                        telegramApi.post('/sendMessage', {
+                            chat_id: `-${group.groupId}`,
+                            text: values.message,
+                        })
+                    }
+                }, delay)
             }
         },
-        [messageInterval, mutate]
+        [messageInterval, mutate, selectedWeekdays]
     )
+
     return (
         <div className="flex flex-col items-center justify-center bg-blue-500 w-full">
             <h1 className="text-2xl font-bold text-white drop-shadow-md mb-12">
@@ -55,7 +152,7 @@ const Message = () => {
             >
                 <Form.Item
                     label={<span className="text-black">Xabar matni</span>}
-                    name={'message'}
+                    name="message"
                     rules={[
                         {
                             required: true,
@@ -68,42 +165,47 @@ const Message = () => {
                         className="bg-transparent text-black border-2 border-gray-300 rounded-lg p-2"
                     />
                 </Form.Item>
-                <Form.Item name={'interval'} label="Interval">
+
+                <Form.Item name="interval" label="Interval">
                     <Select
-                        onChange={(value) => {
-                            console.log(value)
-                            setMessageInterval(value)
-                        }}
+                        onChange={setMessageInterval}
+                        defaultValue={messageInterval}
                     >
                         <Select.Option value="ONCE">Bir marta</Select.Option>
                         <Select.Option value="HOURLY">Har soatda</Select.Option>
-                        <Select.Option value="DAYLY">Har kuni</Select.Option>
+                        <Select.Option value="DAILY">Har kuni</Select.Option>
                         <Select.Option value="WEEKLY">
                             Hafta kunlari
                         </Select.Option>
                     </Select>
                 </Form.Item>
+
                 {messageInterval === 'WEEKLY' && (
-                    <Form.Item label="Hafta kunlari" name={'weekdays'}>
+                    <Form.Item label="Hafta kunlari" name="weekdays">
                         <Select
                             mode="multiple"
-                            onChange={(value) => setSelectedWeekdays(value)}
+                            onChange={setSelectedWeekdays}
                             defaultValue={selectedWeekdays}
                         >
-                            <Select.Option value="HOURLY">
-                                Har soatda
-                            </Select.Option>
-                            <Select.Option value="DAYLY">
-                                Har kuni
-                            </Select.Option>
-                            <Select.Option value="WEEKLY">
-                                Hafta kunlari
-                            </Select.Option>
+                            {[
+                                'MONDAY',
+                                'TUESDAY',
+                                'WEDNESDAY',
+                                'THURSDAY',
+                                'FRIDAY',
+                                'SATURDAY',
+                                'SUNDAY',
+                            ].map((day) => (
+                                <Select.Option key={day} value={day}>
+                                    {day}
+                                </Select.Option>
+                            ))}
                         </Select>
                     </Form.Item>
                 )}
+
                 <Form.Item
-                    name={'sendTime'}
+                    name="sendTime"
                     label="Yuborish vaqti"
                     rules={[
                         {
@@ -112,8 +214,16 @@ const Message = () => {
                         },
                     ]}
                 >
-                    <TimePicker className="w-full" showSecond={false} />
+                    <TimePicker
+                        onChange={(value) =>
+                            setSendTime(value?.format('HH:mm'))
+                        }
+                        className="w-full"
+                        format="HH:mm"
+                        showSecond={false}
+                    />
                 </Form.Item>
+
                 <Form.Item>
                     <Button
                         htmlType="submit"
@@ -124,7 +234,8 @@ const Message = () => {
                         Yuborish
                     </Button>
                 </Form.Item>
-                <Link to={'/'}>
+
+                <Link to="/">
                     <Button
                         size="large"
                         className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded-lg w-full md:w-auto"
