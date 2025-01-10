3d0199cd8c8b276e84c6ae80339e3172787b66fc
lastforever457 <lastforever457@gmail.com>
2025-01-10 22:22:31 +0500

    blocking route

--- src/pages/permission-denied.tsx
+++ src/pages/permission-denied.tsx
@@ -1,6 +1,6 @@
 const PermissionDenied = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-full">
+    <div className="flex flex-col text-white items-center justify-center h-full">
       <h1 className="text-4xl font-bold mb-4">Ruxsat yo&apos;q</h1>
       <p className="text-xl">
         Siz bu sahifaga kirish uchun ruxsatga ega emassiz. Iltimos, tizim
