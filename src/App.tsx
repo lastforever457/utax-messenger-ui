c31b0f117d0ab21707d6ef5e481fcf89db91c8e2
lastforever457 <lastforever457@gmail.com>
2025-01-10 22:03:40 +0500

    user id in web app

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import ActiveMessages from './pages/active-messages'
 import Group from './pages/group'
@@ -7,18 +7,27 @@ import Message from './pages/message'
 import Photo from './pages/photo'
 
 function App() {
+    const [currentUserId, setCurrentUserId] = useState<string | number | null>(
+        null
+    )
+
     useEffect(() => {
-        const tg = window.Telegram.WebApp
+        const tg = window.Telegram.WebApp as any
+
+        // Foydalanuvchi ma'lumotlarini olish
+        const user = tg.initDataUnsafe?.user
+        if (user) {
+            setCurrentUserId(user.id) // Foydalanuvchi ID sini saqlash
+        }
 
         tg.ready()
-
-        console.log('Viewport height:', tg.viewportHeight)
-
-        tg.expand() // To'liq ekranga o'chiriladi
     }, [])
 
     return (
         <div className="min-h-dvh px-5 py-10 flex flex-col items-center justify-center bg-blue-500">
+            <h1 className="text-white mb-5">
+                Foydalanuvchi ID: {currentUserId}
+            </h1>
             <Routes>
                 <Route path="/" element={<Main />} />
                 <Route path="/message" element={<Message />} />
